240e192087dc660b567d44e46f185991fbf5ad10
James Uffindell <james.n.uffindell@gmail.com>
2017-02-20 18:25:17 +0000

    add simple abstract base impl of IRestRequest

---
+++ b2-csharp-client/B2.Client/Rest/RestRequest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+
+
+namespace B2.Client.Rest
+{
+    public abstract class RestRequest : IRestRequest
+    {
+        public HttpMethod Method { get; }
+        public IList<Param> HeaderParameters { get; }
+        public IList<Param> QueryParameters { get; }
+        public IList<Param> UrlParameters { get; }
+        public IList<RequestData> BodyParameters { get; }
+
+
+        protected RestRequest(HttpMethod method, IList<Param> headerParameters, IList<Param> queryParameters, IList<Param> urlParameters,
+                           IList<Param> bodyParameters, IList<RequestData> dataParameters)
+        {
+            Method = method;
+            HeaderParameters = headerParameters.ThrowIfNull(nameof(headerParameters));
+            QueryParameters = queryParameters.ThrowIfNull(nameof(queryParameters));
+            UrlParameters = urlParameters.ThrowIfNull(nameof(urlParameters));
+            BodyParameters = bodyParameters.ThrowIfNull(nameof(bodyParameters))
+                                           .Select(p => new FieldRequestData(p.Name, p.Value))
+                                           .Concat(dataParameters.ThrowIfNull(nameof(dataParameters)))
+                                           .ToList();
+        }
+
+
+        public abstract HttpRequestMessage ToHttpRequestMessage(IList<UrlSegment> urlSegments);
+
+        public abstract string BuildUrl(IList<UrlSegment> urlSegments);
+    }
+}
