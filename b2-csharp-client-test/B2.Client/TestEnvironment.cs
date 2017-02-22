b94a57b10bd43cb382c40709dd518acc916acf6d
Richard D.G. Hickman <rdghickman@idnet.com>
2017-02-22 20:51:10 +0000

    add TestEnvironment utility class

---
+++ b2-csharp-client-test/B2.Client/TestEnvironment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace B2.Client.Test
+{
+    internal static class TestEnvironment
+    {
+        internal const string B2_API_URL = "https://api.backblazeb2.com";
+
+
+        internal static string GetTestConfiguration(string key)
+        {
+            var ret = Environment.GetEnvironmentVariable(key);
+            if (ret == null) {
+                Assert.Inconclusive();
+            }
+            return ret;
+        }
+
+        internal static string GetTestAccountId() => GetTestConfiguration("B2_ACCOUNT_ID");
+
+        internal static string GetTestAccountKey() => GetTestConfiguration("B2_ACCOUNT_KEY");
+    }
+}
