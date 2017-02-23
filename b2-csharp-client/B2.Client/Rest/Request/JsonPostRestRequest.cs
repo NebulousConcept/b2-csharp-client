2d20e6d785789a19d8a7d68456a404f502f90d04
James Uffindell <james.n.uffindell@gmail.com>
2017-02-23 19:02:12 +0000

    add JSON-encoded request formats

---
+++ b2-csharp-client/B2.Client/Rest/Request/JsonPostRestRequest.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+
+using B2.Client.Rest.Request.Param;
+
+
+namespace B2.Client.Rest.Request
+{
+    /// <summary>
+    /// A <see cref="JsonRestRequest"/> using HTTP POST.
+    /// </summary>
+    public abstract class JsonPostRestRequest : JsonRestRequest
+    {
+        /// <summary>
+        /// Create a new <see cref="JsonPostRestRequest"/>.
+        /// </summary>
+        /// <param name="urlParameters">The URL parameters.</param>
+        /// <param name="headers">The header parameters.</param>
+        /// <param name="body">The body parameters.</param>
+        protected JsonPostRestRequest(UrlParams urlParameters, HeaderParams headers, BodyParams body)
+            : base(HttpMethod.Post, headers, urlParameters, body) { }
+    }
+}
---
+++ b2-csharp-client/B2.Client/Rest/Request/JsonRestRequest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+using Newtonsoft.Json.Linq;
+
+using B2.Client.Rest.Request.Param;
+
+
+namespace B2.Client.Rest.Request
+{
+    /// <summary>
+    /// A REST request whose body is a JSON object containing the parameters.
+    /// </summary>
+    public abstract class JsonRestRequest : RestRequest
+    {
+        /// <summary>
+        /// Create a new REST request whose body is a JSON object containing the parameters.
+        /// </summary>
+        /// <param name="method">The method to use.</param>
+        /// <param name="headers">The header parameters.</param>
+        /// <param name="urlParameters">The URL parameters.</param>
+        /// <param name="body">The body parameters.</param>
+        protected JsonRestRequest(HttpMethod method, HeaderParams headers, UrlParams urlParameters, BodyParams body)
+            : base(method, headers, QueryParams.Empty, urlParameters, body, DataParams.Empty) { }
+
+
+        /// <inheritdoc/>
+        public override HttpRequestMessage ToHttpRequestMessage(IEnumerable<UrlSegment> urlSegments)
+        {
+            var ret = base.ToHttpRequestMessage(urlSegments);
+
+            var json = new JObject();
+            foreach (var param in BodyParameters) {
+                foreach (var item in param.Items) {
+                    var pair = item.GetAsKeyValuePair();
+                    json.Add(pair.Key, pair.Value);
+                }
+            }
+
+            ret.Content = new StringContent(json.ToString(), Encoding.UTF8, "application/json");
+            return ret;
+        }
+    }
+}
