89a3ca993ad241c5b52096bb45f6a659a684adad
James Uffindell <james.n.uffindell@gmail.com>
2017-02-23 19:14:20 +0000

    add integration test for b2_authorize_account API

---
+++ b2-csharp-client-test/B2.Client/Apis/AuthorizeAccountV1/TestAuthorizeAccountV1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+using B2.Client.Rest;
+using B2.Client.Test;
+
+
+namespace B2.Client.Apis.AuthorizeAccountV1.Test
+{
+    [TestFixture]
+    public class TestAuthorizeAccountV1
+    {
+        [Test]
+        public async Task TestAuthorizeAccountSuccess()
+        {
+            var client = new UnauthenticatedRestClient(new Uri(TestEnvironment.B2ApiUrl));
+            var req = new AuthorizeAccountV1Request(TestEnvironment.GetTestAccountId(), TestEnvironment.GetTestAccountKey());
+            var result = await client.PerformAuthenticationRequestAsync(new AuthorizeAccountV1Api(), req);
+
+            Assert.That(result.AccountId, Is.EqualTo(TestEnvironment.GetTestAccountId()));
+            Assert.That(result.AuthorizationToken, Is.Not.Null);
+            Assert.That(result.DownloadUrl, Is.Not.Null);
+        }
+    }
+}
