e9030dd79bb80ca40d06cf4db3f35e12a7a1652f
James N. Uffindell <james.n.uffindell@gmail.com>
2017-02-26 15:22:51 +0000

    add b2_delete_bucket API

---
+++ b2-csharp-client/B2.Client/Apis/DeleteBucketV1/DeleteBucketV1Api.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+using B2.Client.Rest;
+using B2.Client.Rest.Api;
+
+
+namespace B2.Client.Apis.DeleteBucketV1
+{
+    /// <summary>
+    /// The 'b2_delete_bucket' version 1 API.
+    /// </summary>
+    public sealed class DeleteBucketV1Api : IAuthenticatedApi<DeleteBucketV1Request, DeleteBucketV1Response>
+    {
+        /// <inheritdoc />
+        public IEnumerable<UrlSegment> ResourceUrl { get; } = new List<UrlSegment> {
+            UrlSegment.Literal("b2api"),
+            UrlSegment.Literal("v1"),
+            UrlSegment.Literal("b2_delete_bucket")
+        };
+    }
+}
---
+++ b2-csharp-client/B2.Client/Apis/DeleteBucketV1/DeleteBucketV1Request.cs
@@ -0,0 +1,23 @@
+﻿using B2.Client.Rest.Request;
+using B2.Client.Rest.Request.Param;
+
+
+namespace B2.Client.Apis.DeleteBucketV1
+{
+    /// <summary>
+    /// Request object for the 'b2_delete_bucket' version 1 API.
+    /// </summary>
+    public sealed class DeleteBucketV1Request : JsonPostRestRequest
+    {
+        /// <summary>
+        /// Create a new request object.
+        /// </summary>
+        /// <param name="accountId">The B2 account ID.</param>
+        /// <param name="bucketId">The ID of the bucket to delete.</param>
+        public DeleteBucketV1Request(string accountId, string bucketId)
+            : base(UrlParams.Empty, HeaderParams.Empty, new BodyParams(
+                   RequiredParam.Of("accountId", accountId),
+                   RequiredParam.Of("bucketId", bucketId)
+                   )) { }
+    }
+}
