01ae779201db8cc533ed526a9979eecaee44d6b7
James N. Uffindell <james.n.uffindell@gmail.com>
2017-03-03 21:07:34 +0000

    add 'b2_create_bucket' to the B2APIs class

--- b2-csharp-client/B2.Client/Apis/B2Apis.cs
+++ b2-csharp-client/B2.Client/Apis/B2Apis.cs
@@ -1,4 +1,5 @@
 ﻿using B2.Client.Apis.AuthorizeAccountV1;
+using B2.Client.Apis.CreateBucketV1;
 using B2.Client.Apis.DeleteBucketV1;
 using B2.Client.Apis.ListBucketsV1;
 
@@ -14,6 +15,11 @@ namespace B2.Client.Apis
         /// The 'b2_authorize_account' version 1 API.
         /// </summary>
         public static readonly AuthorizeAccountV1Api AuthorizeAccountV1 = new AuthorizeAccountV1Api();
+
+        /// <summary>
+        /// The 'b2_create_bucket' version 1 API.
+        /// </summary>
+        public static readonly CreateBucketV1Api CreateBucketV1 = new CreateBucketV1Api();
 
         /// <summary>
         /// The 'b2_delete_bucket' version 1 API.
