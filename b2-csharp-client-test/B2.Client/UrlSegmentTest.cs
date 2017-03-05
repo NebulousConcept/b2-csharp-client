77b8a843a4343b7f34ace1af2fec5bccf89a2796
James N. Uffindell <james.n.uffindell@gmail.com>
2017-03-05 16:51:25 +0000

    add more UrlSegment tests

--- b2-csharp-client-test/B2.Client/UrlSegmentTest.cs
+++ b2-csharp-client-test/B2.Client/UrlSegmentTest.cs
@@ -22,6 +22,24 @@ namespace B2.Client.Test
         }
 
         [Test]
+        public void TestLiteralSegmentDoesNotThrow()
+        {
+            const string paramName = "paramName";
+            const string paramValue = "paramValue";
+            const string segmentName = "segmentName";
+            var param = new RestParam(paramName, paramValue);
+
+            var segment = UrlSegment.Literal(segmentName);
+
+            //check that passing in parameters where none match is okay
+            Assert.That(segment.Transform(param.Yield()), Is.EqualTo(segmentName));
+
+            var conflictingParam = new RestParam(segmentName, paramValue);
+            //check that multiple matching params are okay
+            Assert.That(segment.Transform(conflictingParam.ConcatWith(conflictingParam)), Is.EqualTo(segmentName));
+        }
+
+        [Test]
         public void TestParameterSegmentDoesTransform()
         {
             const string paramName = "paramName";
@@ -31,6 +49,32 @@ namespace B2.Client.Test
             var segment = UrlSegment.Parameter(paramName);
 
             Assert.That(segment.Transform(param.Yield()), Is.EqualTo(paramValue));
+        }
+
+        [Test]
+        public void TestParameterSegmentWithNoMatchThrows()
+        {
+            const string paramName = "paramName";
+            const string paramValue = "paramValue";
+            const string segmentName = "segmentName";
+            var param = new RestParam(paramName, paramValue);
+
+            var segment = UrlSegment.Parameter(segmentName);
+
+            Assert.That(() => segment.Transform(param.Yield()), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void TestParameterSegmentWithMultipleMatchesThrows()
+        {
+            const string paramName = "paramName";
+            const string paramValue = "paramValue";
+
+            var param = new RestParam(paramName, paramValue);
+
+            var segment = UrlSegment.Parameter(paramName);
+
+            Assert.That(() => segment.Transform(param.ConcatWith(param)), Throws.InvalidOperationException);
         }
     }
 }
